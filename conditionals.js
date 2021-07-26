3639597963fa4c611fabd960450e8fd53feab5d1
SJ <sj.jega2@gmail.com>
2021-07-26 22:21:22 +0000

    even.js commit

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,12 @@
-if (!raining) {
-  console.log("Leave your umbrella at home!");
+const whichSchool  = function (age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
 }
+
+console.log (whichSchool (18));
+
