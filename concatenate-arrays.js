ce6913bf81f902ef2d196ad08a981bca1530cf83
SJ <sj.jega2@gmail.com>
2021-07-27 17:39:02 +0000

    1st commit of concatenate-arrays.js

---
+++ concatenate-arrays.js
@@ -0,0 +1,13 @@
+const concat = function(array1, array2) {
+  let array3 = array1;
+  for (let i = 0; i < array2.length; i++) {
+    array3.push(array2[i]);
+  }
+  return array3;
+}
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
